88d0100c6219bb2d5b2f7938e75c3b67e1e33b4b
Kevin Bache <kevin.bache@gmail.com>
2015-04-23 16:55:00 -0700

    updates to mystery data

--- data/generate_overfitting_data.py
+++ data/generate_overfitting_data.py
@@ -95,11 +95,11 @@ with np.load('mystery_data.npz') as data:
     y = data['y']
 
 from sklearn.cross_validation import train_test_split
-x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=4)
+x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=2)
 
 # save temporalized version of data
 np.savez('mystery_data_old.npz', x_old=x_train, y_old=y_train, x_new=x_test)
-np.savez('mystery_data_later.npz', y_new=y_test)
+np.savez('mystery_data_new_y.npz', y_new=y_test)
 
 
 
